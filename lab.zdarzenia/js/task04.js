8b5020d4a92e760fab2dab371551c21916e0cc61
Dariusz Drazek <dariusz.drazek88@gmail.com>
2019-07-03 19:16:10 +0200

    test commit

--- lab.zdarzenia/js/task04.js
+++ lab.zdarzenia/js/task04.js
@@ -1,19 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-
-
-
-
-
 /**
  * Nie modyfikujcie kodu poniżej
  */
 
 function App() {
-  return <EnterAndLeave />;
+     return <EnterAndLeave />;
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 export { App, EnterAndLeave };
